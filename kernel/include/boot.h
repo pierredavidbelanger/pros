b25eac315cbe06c6a3023227ba4597e14a99d972
Pierre-David Belanger <pierredavidbelanger@gmail.com>
2026-08-02 23:19:05 -0400

    cleanup

---
+++ kernel/include/boot.h
@@ -0,0 +1,34 @@
+#ifndef PROS_BOOT_H
+#define PROS_BOOT_H
+
+#include <limine.h>
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0
+};
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_dtb_request dtb_request = {
+    .id = LIMINE_DTB_REQUEST_ID,
+    .revision = 0
+};
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_stack_size_request stack_size_request = {
+    .id = LIMINE_STACK_SIZE_REQUEST_ID,
+    .revision = 0,
+    .stack_size = 64 * 1024 // 64 KiB stack
+};
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+
+#endif //PROS_BOOT_H
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,61 +1,14 @@
 #include "arch.h"
+#include "boot.h"
 #include "fb.h"
 #include "kprintf.h"
 
-#include <limine.h>
-
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-
-// Set the base revision to 6, this is recommended as this is the latest
-// base revision described by the Limine boot protocol specification.
-// See specification for further info.
-
-__attribute__((used, section(".limine_requests")))
-static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
-
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent, _and_ they should be accessed at least
-// once or marked as used with the "used" attribute as done here.
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
-    .revision = 0
-};
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_dtb_request dtb_request = {
-    .id = LIMINE_DTB_REQUEST_ID,
-    .revision = 0
-};
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_stack_size_request stack_size_request = {
-    .id = LIMINE_STACK_SIZE_REQUEST_ID,
-    .revision = 0,
-    .stack_size = 64 * 1024  // 64 KiB stack
-};
-
-// Finally, define the start and end markers for the Limine requests.
-// These can also be moved anywhere, to any .c file, as seen fit.
-
-__attribute__((used, section(".limine_requests_start")))
-static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
-
-__attribute__((used, section(".limine_requests_end")))
-static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
-
 void kmain(void) {
-
-    arch_init();
-
-    if (LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false) {
+    if (!LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision)) {
         khcf();
     }
 
+    arch_init();
     fb_init(framebuffer_request.response);
 
     kprintf("Welcome to PjErOS!");
