e221034734f5deda6e0c10c16db9a9667705a5a5
Pierre-David Belanger <pierredavidbelanger@gmail.com>
2026-08-03 19:03:10 -0400

    aarch64 compile again

--- kernel/arch/aarch64/config.mk
+++ kernel/arch/aarch64/config.mk
@@ -1,3 +1,3 @@
 CC=zig cc -target aarch64-freestanding-none
-CFLAGS=-mcpu=generic -march=armv8-a+nofp+nosimd -mgeneral-regs-only
-LDFLAGS=-m aarch64elf -T arch/aarch64/linker.lds
+CFLAGS=-mcpu=generic -mgeneral-regs-only
+LDFLAGS=
