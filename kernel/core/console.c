1b7a580f50200d5f20e58bb8dc74b70ef48428a3
Pierre-David Belanger <pierredavidbelanger@gmail.com>
2026-08-08 12:35:59 -0400

    clear the terminal on boot

--- kernel/core/console.c
+++ kernel/core/console.c
@@ -10,6 +10,11 @@ static bool earlycon_active = false;
 void console_init(void) {
     earlycon_init();
     earlycon_active = true;
+    // clear the terminal so kernel output starts on a clean line instead of overlapping
+    const char *clear_seq = "\033[2J\033[H";
+    for (const char *p = clear_seq; *p; p++) {
+        console_putc(*p);
+    }
 }
 
 void console_putc(char c) {
